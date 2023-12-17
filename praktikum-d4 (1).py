2cedc4a845f6ea12d54bfd573b8c2223590fd0ff
corneto21 <153706486+jrii112@users.noreply.github.com>
2023-12-17 23:36:40 +0700

    upload file 4

---
+++ praktikum-d4 (1).py
@@ -0,0 +1,66 @@
+a = [2,3,1,0,3,1,1,0,1]
+# akses item
+print(a)
+print(f'item indeks:0 {a[0]}')
+print(f'item indeks:3 {a[1]}')
+print(f'item indeks:pertama {a[len(a)-1]}')
+# akses item indeks negatif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(f'item indeks:5 (-4) {a[3:]}')
+# akses indeks batas
+print(f'item indeks:1,2,3 {a[1:4]}')
+print(f'item indeks:1,2... {a[1:]}')
+print(f'item indeks:3,4... {a[3:]}')
+print(f'item indeks:0,1,2 {a[:3]}')
+print(f'item indeks:0,1,2,3,4 {a[:5]}')
+print(f'item indeks:semua {a[]}')
+# pengiringan indeks
+print(f'item indeks:1,2,3 {a[1:4]}')
+print(f'item indeks:2,3,4 {a[2:5]}')
+print(f'item indeks:[1:8] {a[1:-1]}')
+
+# Nested list
+kelas = [('Matkul agama islam',3),
+         ('Matkul pancasila',2),
+         ('Matkul pengantar pemrograman',2)
+         ]
+print(f'data kelas {kelas}')
+kelas.append(('matkul kalkulus',2))
+print(f'data kelas {kelas}')
+
+# Nama mata kuliah 1: agama islam dengan jumlah sks:3
+# Nama mata kuliah 2: pancasila dengan jumlah sks:2
+# Nama mata kuliah 3:  dengan jumlah sks:
+
+
+
+data = [('Fajrin',2023,'aktif'),
+        (90,89,93,97,100),
+        (20,22),
+        ('S1-Reguler','Sistem Informasi D','Ganjil')]
+
+
+
+
+
+
+
+
+
