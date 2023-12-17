288935dbab45a6a21cb01963c77d85b564790baa
corneto21 <153706486+jrii112@users.noreply.github.com>
2023-12-17 23:37:44 +0700

    upload file 5

---
+++ praktikum-d5.py
@@ -0,0 +1,12 @@
+a = True
+i = 0
+limit = 9
+
+while a:
+    i +=1     #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung',10-i)
+    else:
+        print('Program berhenti disini !!!!')
+        a = False
+        
